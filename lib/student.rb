eb88febcb672e4be66d96e9ce7bb25f465843b9b
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-08-14 09:27:36 -0400

    updated find_by_name

--- lib/student.rb
+++ lib/student.rb
@@ -64,9 +64,12 @@ class Student
           SELECT * FROM students
           WHERE name = ?
           SQL
-    result = DB[:conn].execute(sql, name)[0]
-
-    Student.new(result[0], result[1], result[2])
+    # result = DB[:conn].execute(sql, name)[0]
+    # Student.new(result[0], result[1], result[2])
+    
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
